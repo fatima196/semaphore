1f912c74d66ef8c12fe4daedf1473fa2f33c34ca
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:45:48 +0200

    Update Router.java

--- Router.java
+++ Router.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package network;
 
 import static java.lang.Thread.sleep;
 
-/**
- *
- * @author lenovo
- */
+
 public class Router {
 
     public boolean[] connectionArray;
