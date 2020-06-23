b00f2b737f40ea3514b9c677ce6fc3aa6cfedc35
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:46:38 +0200

    Update network.java

--- network.java
+++ network.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package network;
 
 import java.io.File;
@@ -11,10 +7,7 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author lenovo
- */
+
 public class network {
 
     /**
